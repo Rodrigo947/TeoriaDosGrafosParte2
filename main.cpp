cf2b40808357cd25830367465f5b332f62906f77
IanCouto <48738352+IanCouto@users.noreply.github.com>
2020-03-04 15:57:54 -0300

    corrige parâmetro para randomizaAlfa

--- main.cpp
+++ main.cpp
@@ -197,10 +197,13 @@ int main(int argc, char* argv[]) {
 
 	Grafo* grafo = new Grafo();
 
-    if(arquivo_entrada.is_open())
+    if(arquivo_entrada.is_open() )
         grafo = leitura(arquivo_entrada);
-    else
+    else{
         cout << "Nao foi possivel abrir o arquivo " << argv[1] << endl;
+        return 0;
+    }
+
 
 
     int selecao = menu();
